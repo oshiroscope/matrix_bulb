77d3a405217638c787c9e2710bcbee5bdbc55d12
oshiroscope <whitecastle4646@gmail.com>
2017-01-04 14:12:31 +0900

    add debug print

--- matrix_bulb.ino
+++ matrix_bulb.ino
@@ -4,36 +4,104 @@
 
 #define MAX_BULB_NUM 5
 
-#define ROW 5
-#define COLUMN 8
+#define ROW 3
+#define COLUMN 10
 
-int ids[MAX_BULB_NUM];
+struct Pos
+{
+public:
+  int col;
+  int row;
+};
 
+int pos2Id(Pos pos)
+{
+  return pos.col + pos.row * COLUMN + 2;
+}
+
+Pos bulbs[MAX_BULB_NUM];
 
 // duty : 0.0 ~ 1.0
-void oneCyclePwmCtrl(int *_ids, int _num, float _duty)
+void oneCyclePwmCtrl(Pos *_bulbs, int _num, float _duty)
 {
+  Serial.println(_duty);
+
   int time_on = LOOP_PERIOD_MICROS * _duty;
   int time_off = LOOP_PERIOD_MICROS - time_on;
   if(time_on > 0){
     for(int i = 0; i < _num; i++)
     {
-      digitalWrite(_ids[i], HIGH);
+      Serial.print(_bulbs[i].row);
+      Serial.print("\t");
+      Serial.print(_bulbs[i].col);
+      Serial.print("\t");
+      Serial.print(pos2Id(_bulbs[i]));
+      Serial.println();
+
+      digitalWrite(pos2Id(_bulbs[i]), HIGH);
     }
-    delayMicroseconds(time_on);
+    //delayMicroseconds(time_on);
+    delay(time_on / 1000);
     for(int i = 0; i < _num; i++)
     {
-      digitalWrite(_ids[i], LOW);
+      digitalWrite(pos2Id(_bulbs[i]), LOW);
     }
-    delayMicroseconds(time_off);
+    //delayMicroseconds(time_off);
+    delay(time_off / 1000);
   }
+}
+
+void light(Pos *_bulbs, int _num)
+{
+  int cnt = 0;
+  while(1)
+  {
+    oneCyclePwmCtrl(_bulbs, _num, 0.8);
+    cnt++;
+    if(cnt == 13)
+    {
+      break;
+    }
+  }
+  cnt = 5;
+  while(1)
+  {
+    int max_cnt = 400;
+    oneCyclePwmCtrl(_bulbs, _num, 5 / (float)cnt);
+    cnt++;
+    if(cnt == max_cnt)
+    {
+      break;
+    }
+  }
+  cnt = 0;
+  while(1)
+  {
+    int max_cnt = 100;
+    oneCyclePwmCtrl(_bulbs, _num, (5 / (float)400) * ((float)max_cnt - cnt) / max_cnt);
+    cnt++;
+    if(cnt == max_cnt)
+    {
+      break;
+    }
+  }
+
 }
 
 void setup() {
   Serial.begin(BAUDRATE);
   randomSeed(analogRead(0));
 
-  //TODO setup GPIO
+  for(int i = 0; i < COLUMN; i++)
+  {
+    for(int j = 0; j < ROW; j++)
+    {
+      Pos p;
+      p.col = i;
+      p.row = j;
+      pinMode(pos2Id(p), OUTPUT);
+    }
+  }
 }
 
 void loop() {
@@ -41,7 +109,9 @@ void loop() {
   if(true){
     int row = random(ROW);
     int column = random(COLUMN);
-
+    bulbs[0].row = row;
+    bulbs[0].col = column;
+    light(bulbs, 1);
   }
 
 }
