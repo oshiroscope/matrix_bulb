c8236e542226d6b1d4e8f834af442c5212697992
oshiroscope <whitecastle4646@gmail.com>
2017-01-06 00:48:18 +0900

    refactor file

--- params.h
+++ params.h
@@ -1,3 +1,14 @@
+#pragma once
+
+#define BAUDRATE 115200
+#define LOOP_PERIOD_MS 20
+#define LOOP_PERIOD_MICROS 20000
+
+#define MAX_BULB_NUM 5
+
+#define ROW 3
+#define COLUMN 10
+
 #define DUTY_TABLE_SIZE 500
 
 extern float duty_table[DUTY_TABLE_SIZE];
